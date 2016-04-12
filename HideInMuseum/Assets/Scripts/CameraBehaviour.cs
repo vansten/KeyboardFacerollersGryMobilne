7e9aa496bc147b4fb178edc5a3e0795c410f095e
Krzysztof Stengert <krzysztof.stengert@hotmail.com>
2016-04-12 15:26:12 +0200

    [*] Reworked camera behaviour [*] Changed game name

--- HideInMuseum/Assets/Scripts/CameraBehaviour.cs
+++ HideInMuseum/Assets/Scripts/CameraBehaviour.cs
@@ -8,31 +8,36 @@ public class CameraBehaviour : MonoBehaviour
     [SerializeField]
     private Transform _maxRestriction;
     [SerializeField]
-    private Transform _target;
-    [SerializeField]
-    private Vector3 _offset;
+    private bool _inverted;
 
-    private CharacterMovement _currentCharacterAffected;
-    private Vector3 _newPosition;
-    private Vector3 _cameraPositionBeforeLerp;
-    private float _lerpTimer = 0.0f;
-
-    void Start()
-    {
-        transform.position = _target.position + _offset;
-    }
+    private CuratorMovement _currentCharacterAffected;
+    private Vector3 _previousMousePosition;
+    private Vector3 _deltaMousePosition;
+    private bool _isDown = false;
+    private bool _mouseDown = false;
+    private bool _mouseBeingHeldDown = false;
+    private bool _mouseUp = false;
 
     void Update()
     {
-        if (Input.GetMouseButton(0))
+        _mouseDown = Input.GetMouseButtonDown(0);
+        _mouseBeingHeldDown = Input.GetMouseButton(0);
+        _mouseUp = Input.GetMouseButtonUp(0);
+        ProcessCuratorsMovement();
+        ProcessCameraScrolling();
+    }
+
+    void ProcessCuratorsMovement()
+    {
+        if(_mouseDown)
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
-            if (hit.collider != null)
+            if(hit.collider != null)
             {
                 if (_currentCharacterAffected == null)
                 {
-                    _currentCharacterAffected = hit.collider.gameObject.GetComponent<CharacterMovement>();
+                    _currentCharacterAffected = hit.collider.gameObject.GetComponent<CuratorMovement>();
                     if (_currentCharacterAffected != null)
                     {
                         Vector3 position;
@@ -41,30 +46,33 @@ public class CameraBehaviour : MonoBehaviour
                         _currentCharacterAffected.StartPath(position);
                     }
                 }
-                else
+            }
+        }
+        else if(_mouseBeingHeldDown)
+        {
+            if(_currentCharacterAffected != null)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+                if (hit.collider != null)
                 {
                     //Check if sth was hit or just continue
-                    if(hit.collider.gameObject.layer == LayerMask.NameToLayer("Obstacle"))
+                    if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Obstacle"))
                     {
                         _currentCharacterAffected.WrongPath();
                     }
-
-                    Vector3 position;
-                    position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                    position.z = 0.0f;
-                    _currentCharacterAffected.ContinuePath(position);
                 }
-            }
-            else if(_currentCharacterAffected != null)
-            {
+
                 Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
                 position.z = 0.0f;
                 _currentCharacterAffected.ContinuePath(position);
             }
         }
-        else if(Input.GetMouseButtonUp(0))
+        else if(_mouseUp)
         {
-            if(_currentCharacterAffected != null)
+            _deltaMousePosition = Vector3.zero;
+
+            if (_currentCharacterAffected != null)
             {
                 Vector3 position;
                 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -75,22 +83,31 @@ public class CameraBehaviour : MonoBehaviour
         }
     }
 
-    void LateUpdate()
+    void ProcessCameraScrolling()
     {
-        Vector2 targetPositionXY = new Vector2(_target.position.x, _target.position.y);
-        Vector2 cameraPositionXY = new Vector2(transform.position.x, transform.position.y);
-        if (Mathf.Abs(targetPositionXY.x - cameraPositionXY.x) < 1.0f && (Mathf.Abs(targetPositionXY.y - cameraPositionXY.y) < 1.0f))
+        if(_currentCharacterAffected != null)
         {
-            _cameraPositionBeforeLerp = transform.position;
-            _lerpTimer = 0.0f;
+            _previousMousePosition = Input.mousePosition;
             return;
         }
 
-        _lerpTimer += Time.deltaTime;
+        if (!_mouseBeingHeldDown && !_mouseDown)
+        {
+            _previousMousePosition = Input.mousePosition;
+        }
+        _deltaMousePosition = Input.mousePosition - _previousMousePosition;
+        if(_inverted)
+        {
+            _deltaMousePosition = -_deltaMousePosition;
+        }
+        _previousMousePosition = Input.mousePosition;
+    }
 
-        _newPosition = _target.position + _offset;
-        _newPosition.x = Mathf.Clamp(_newPosition.x, _minRestriction.position.x, _maxRestriction.position.x);
-        _newPosition.y = Mathf.Clamp(_newPosition.y, _minRestriction.position.y, _maxRestriction.position.y);
-        transform.position = Vector3.Lerp(_cameraPositionBeforeLerp, _newPosition, _lerpTimer);
+    void LateUpdate()
+    {
+        Vector3 newPosition = transform.position + _deltaMousePosition * Time.deltaTime;
+        newPosition.x = Mathf.Clamp(newPosition.x, _minRestriction.position.x, _maxRestriction.position.x);
+        newPosition.y = Mathf.Clamp(newPosition.y, _minRestriction.position.y, _maxRestriction.position.y);
+        transform.position = newPosition;
     }
 }
---
+++ HideInMuseum/Assets/Scripts/CuratorMovement.cs
@@ -0,0 +1,152 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CuratorMovement : MonoBehaviour
+{
+    [SerializeField]
+    private LineRenderer _lineRenderer;
+    [SerializeField]
+    private float _speed;
+    [SerializeField]
+    private float _tolerance;
+
+    private List<Vector3> _path = new List<Vector3>();
+    private List<Vector3> _lineRendererPoints = new List<Vector3>();
+    private Color _colorToSet = Color.white;
+    private Vector3 _lastPosition;
+    private Vector3 _lastLineRendererPosition;
+    private float _moveTimer;
+    private float _distanceToNextNode = 0.0f;
+    private int _currentPathIndex = 0;
+    private int _positionsCount = 0;
+    private bool _isMoving;
+
+    void Start()
+    {
+        _lineRenderer.sortingOrder = 2;
+    }
+
+    void Update()
+    {
+        if(_isMoving)
+        {
+            if(_currentPathIndex < _path.Count)
+            {
+                _moveTimer += Time.deltaTime * _speed / _distanceToNextNode;
+                transform.position = Vector3.Lerp(_lastPosition, _path[_currentPathIndex], _moveTimer);
+                UpdateLineRendererPoints();
+                if (_moveTimer >= 1.0f)
+                {
+                    _moveTimer = 0.0f;
+                    _currentPathIndex += 1;
+                    _lastPosition = transform.position;
+                    if (_currentPathIndex >= _path.Count)
+                    {
+                        _isMoving = false;
+                    }
+                    else
+                    {
+                        _distanceToNextNode = Vector3.Distance(_path[_currentPathIndex], _lastPosition);
+                    }
+                }
+            }
+        }
+    }
+
+    void UpdateLineRendererPoints()
+    {
+        int index = 0;
+        float closestDist = float.MaxValue;
+        for(int i = 0; i < _lineRendererPoints.Count; ++i)
+        {
+            float dist = Vector3.Distance(transform.position, _lineRendererPoints[i]);
+            if (dist < closestDist)
+            {
+                index = i;
+                closestDist = dist;
+            }
+        }
+
+        if(closestDist > 0.2f)
+        {
+            return;
+        }
+
+        _lineRendererPoints = _lineRendererPoints.GetRange(index, _lineRendererPoints.Count - index);
+
+        _lineRenderer.SetVertexCount(_lineRendererPoints.Count);
+        _lineRenderer.SetPositions(_lineRendererPoints.ToArray());
+    }
+
+    public void StartPath(Vector3 position)
+    {
+        _isMoving = false;
+        _path.Clear();
+        _positionsCount = 0;
+        Color c = Color.white;
+        c.a = 0.5f;
+        _lineRenderer.SetVertexCount(0);
+        _lineRenderer.SetColors(c, c);
+        _lineRendererPoints.Clear();
+        _lastLineRendererPosition = position;
+        _colorToSet = Color.white;
+    }
+
+    public void ContinuePath(Vector3 position)
+    {
+        if(_path.Count > 0)
+        {
+            if (Vector3.Distance(position,_path[_path.Count - 1]) < _tolerance)
+            {
+                return;
+            }
+        }
+        float dist = Vector3.Distance(_lastLineRendererPosition, position);
+        if(dist > 0.2f)
+        {
+            Vector3 diff = (position - _lastLineRendererPosition);
+            int k = (int)(dist * 10.0f);
+            diff *= 1.0f / k;
+            for(int i = 1; i < k; ++i)
+            {
+                _lineRenderer.SetVertexCount(_positionsCount + 1);
+                _lineRenderer.SetPosition(_positionsCount, _lastLineRendererPosition + diff * i);
+                _lineRendererPoints.Add(_lastLineRendererPosition + diff * i);
+                _positionsCount += 1;
+            }
+        }
+        _lineRenderer.SetVertexCount(_positionsCount + 1);
+        _lineRenderer.SetPosition(_positionsCount, position);
+        _lineRendererPoints.Add(position);
+        _lastLineRendererPosition = position;
+        _positionsCount += 1;
+        _path.Add(position);
+    }
+
+    public void EndPath(Vector3 position)
+    {
+        _lineRenderer.SetColors(_colorToSet, _colorToSet);
+        _path.Add(position);
+        _isMoving = true;
+        _currentPathIndex = 0;
+        _moveTimer = 0.0f;
+        _lastPosition = transform.position;
+        _distanceToNextNode = Vector3.Distance(_path[0], transform.position);
+    }
+
+    public void WrongPath()
+    {
+        _colorToSet = Color.red;
+        Color c = Color.red;
+        c.a = 0.5f;
+        _lineRenderer.SetColors(c, c);
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        _isMoving = false;
+        _path.Clear();
+        _lineRenderer.SetVertexCount(0);
+    }
+}
