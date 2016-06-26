8a90d280077e5a9fc102ba4a7be8e545e773fd8b
Krzysztof Stengert <krzysztof.stengert@hotmail.com>
2016-06-26 21:45:26 +0200

    [*] Shop view adjusted a little bit

--- HideInMuseum/Assets/Scripts/UI/RoomImage.cs
+++ HideInMuseum/Assets/Scripts/UI/RoomImage.cs
@@ -5,9 +5,12 @@ using UnityEngine.UI;
 public class RoomImage : MonoBehaviour
 {
     public Image Image;
-    public GameObject BuyButton;
+    public Button BuyButton;
     public Image Icon;
     public Text Name;
+    public Text MoneyRequired;
+    public Image MoneyImage;
+    public AudioClip PurchaseSound;
 
     private RoomInfo _roomInfo;
 
@@ -17,18 +20,24 @@ public class RoomImage : MonoBehaviour
         Image.sprite = ri.ShopImage;
         Icon.sprite = ri.Icon;
         Name.text = ri.Name;
-        BuyButton.SetActive(!ri.Room.Unlocked);
-        BuyButton.GetComponentInChildren<Text>().text = ri.MoneyToUnlock.ToString();
+        BuyButton.interactable = !ri.Room.Unlocked;
+        MoneyRequired.gameObject.SetActive(!ri.Room.Unlocked);
+        MoneyImage.gameObject.SetActive(!ri.Room.Unlocked);
+        MoneyRequired.text = ri.MoneyToUnlock.ToString();
     }
 
     public void BuyClick()
     {
+        GameManager.Instance.TotalMoney += _roomInfo.MoneyToUnlock;
         if(GameManager.Instance.TotalMoney >= _roomInfo.MoneyToUnlock)
         {
             GameManager.Instance.TotalMoney -= _roomInfo.MoneyToUnlock;
             _roomInfo.Room.Unlocked = true;
-            BuyButton.SetActive(false);
+            BuyButton.interactable = false;
+            MoneyRequired.gameObject.SetActive(false);
+            MoneyImage.gameObject.SetActive(false);
             GameManager.Instance.SaveData();
+            AudioSource.PlayClipAtPoint(PurchaseSound, Camera.main.transform.position, 0.75f);
         }
     }
 }
